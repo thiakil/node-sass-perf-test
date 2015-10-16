ff18adb6d16702275e08e2ceb1b4b9ad9d1b0bba
Xander Victory <x@xandervictory.id.au>
2015-10-16 14:19:03 +0800

    remove superfluous deps

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 var sass = require("node-sass");
-var sassUtils = require("node-sass-utils")(sass);
 
-var result = sass.render({
-  file: "perftest.scss",
+var types = sass.types;
+
+console.time('native');
+var result = sass.renderSync({
+  file: "perftest-native.scss",
+});
+console.timeEnd('native');
+
+console.time('js');
+var result = sass.renderSync({
+  file: "perftest-js.scss",
   functions: {
     "perftest-js($value)": function($value) {
       // just return the value
       return $value;
     },
-
-    // timer function so we can see how long things took
-    "timer($time: null)": function($time) {
-      var currentTime = Date.now();
-      var time = sassUtils.castToJs($time);
-      if (time) {
-        time = currentTime - time.value;
-      }
-      else {
-        time = currentTime;
-      }
-
-      return sassUtils.castToSass(time);
-    }
   }
 });
+console.timeEnd('js');
